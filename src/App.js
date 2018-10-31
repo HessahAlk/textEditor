631ccd1c9cda2d82e737c266f4879a0d398a3c6f
HessahAlK <hessah.alkhudhair@gmail.com>
2018-10-31 16:40:06 +0300

    Task Submission

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import ButtonStyle from "./ButtonStyle";
 
 const styles = {
   bold: { fontWeight: "bold" },
@@ -7,7 +8,51 @@ const styles = {
   underline: { textDecorationLine: "underline" }
 };
 
+
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      styling: {
+        bold: false,
+        italic: false,
+        underline: false
+      },
+      color: "black"
+
+    };
+    this.setStyle = this.setStyle.bind(this);
+    this.chooseColor = this.chooseColor.bind(this);
+  }
+
+  setStyle(style) {
+    const styling = { ...this.state.styling };
+    styling[style] = !styling[style];
+    this.setState({ styling: styling });
+  }
+
+  chooseColor(color) {
+    this.setState({ color: color });
+  }
+
+  createStyleObject() {
+    let style = {}
+    if (this.state.styling['bold']) {
+      style = {...style, ...styles.bold}
+    }
+
+    if (this.state.styling['italic']) {
+      style = {...style, ...styles.italic}
+    }
+
+    if (this.state.styling['underline']) {
+      style = {...style, ...styles.underline}
+    }
+
+    return style
+  }
+
   render() {
     let stylings = ["bold", "italic", "underline"];
     let colors = ["yellow", "blue", "red", "black", "purple"];
@@ -20,27 +65,30 @@ class App extends Component {
       );
     });
 
+
+    // this.colors = ["yellow", "blue", "red", "black", "purple"];
+    //
+
     let colorBoxes = colors.map(color => {
-      return (
-        <button
-          style={{ backgroundColor: color, height: 30, width: 30 }}
-          key={color}
-        />
-      );
+      return <button key={color} onClick={() => this.chooseColor(color)} style={{ backgroundColor: color, height: 30, width: 30 }} />;
     });
 
     return (
       <div className="App">
         <br />
-        {stylingBoxes}
+        <ButtonStyle select={this.setStyle} name="bold" />
+        <ButtonStyle select={this.setStyle} name="italic" />
+        <ButtonStyle select={this.setStyle} name="underline" />
         <br />
         <br />
-        <textarea />
+        <textarea style={{ color: this.state.color, ...this.createStyleObject() }}/>
         <br />
         {colorBoxes}
       </div>
     );
   }
+
 }
+
 
 export default App;
